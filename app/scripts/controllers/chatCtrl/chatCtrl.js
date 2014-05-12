55768bf9735b143dc51836f4b3ce5caf1bd8ebfc
David <dsalme@gmail.com>
2014-05-12 02:47:03 -0300

    chat funcional, tengo que cambiar la forma en la que se setea el nickname.

--- app/scripts/controllers/chatCtrl/chatCtrl.js
+++ app/scripts/controllers/chatCtrl/chatCtrl.js
@@ -2,30 +2,44 @@
  * Created by dsalmeron on 07/05/14.
  */
 
-app.controller('ChatCtrl', ['$scope','socketService', function($scope, socketService){
-    $scope.nickName = "";
+app.controller('ChatCtrl', ['$scope', 'socketService', '$log', function ($scope, socketService, $log) {
+    $scope.nickName = "daveh";
     $scope.message = {
         author: $scope.nickName,
         content: ""
     };
     $scope.messageLog = [];
 
-    $scope.sendMessage = function(message){
+    $scope.sendMessage = function (message) {
         var splitted = message.content.split(" ");
-        if(splitted[0] == "/nickname"){
+        if (splitted[0] == "/nickname") {
             $scope.nickName = "";
-            for(var i = 1; i < splitted.length; i++){
-                $scope.nickName += splitted[i];
-                $scope.message.content = "";
+            for (var i = 0; i < splitted.length; i++) {
+                $scope.nickName += splitted[i + 1];
             }
-        }else {
+            $scope.message.content = "";
+        } else {
             //$scope.messageLog.push(message);
-            socketService.emit('messages/createMessage', message, $scope.addMessage());
+            socketService.emit('/messages/create', message, $scope.addMessage);
             $scope.message.content = "";
         }
     };
 
-    $scope.addMessage = function(message){
-        $scope.messageLog.push(message);
-    }
+    $scope.addMessage = function (message) {
+        //$scope.messageLog.push(message);
+        $log.debug('se dispara la funcion que recibe como callback, con el mensaje' + message);
+    };
+
+    socketService.forward('newMessage', $scope);
+    $scope.$on('socket:newMessage', function (event, data) {
+        $log.debug('got a message', data.author);
+        /*if (!data.content) {
+            $log.error('invalid message', 'data', JSON.stringify(data));
+            return;
+        }*/
+        $scope.$apply(function () {
+            $scope.messageLog.push(data);
+            //log$scope.messageLog = messageFormatter(new Date(), data.source, data.payload) + $scope.messageLog;
+        });
+    });
 }]);
--- app/scripts/services/socketService.js
+++ app/scripts/services/socketService.js
@@ -1,7 +1,12 @@
 'use strict';
 app.factory('socketService', function (socketFactory) {
-    io.connect('http://localhost:3000');
-    var socket = socketFactory();
-    //socket.forward('broadcast');
-    return socket;
+
+    var myIoSocket = io.connect('http://192.168.2.109:3000');
+
+    var mySocket = socketFactory({
+        ioSocket: myIoSocket
+    });
+    mySocket.forward('broadcast');
+
+    return mySocket;
 });
