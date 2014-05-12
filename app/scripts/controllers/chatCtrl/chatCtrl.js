8642643a8d42e455bc1abe4d7fbc4fc8f94ff144
David Salmeron <dsalme@gmail.com>
2014-05-12 13:42:34 -0300

    agregue bootstrap para probar vs foundation

--- app/scripts/controllers/chatCtrl/chatCtrl.js
+++ app/scripts/controllers/chatCtrl/chatCtrl.js
@@ -3,43 +3,42 @@
  */
 
 app.controller('ChatCtrl', ['$scope', 'socketService', '$log', function ($scope, socketService, $log) {
-    $scope.nickName = "daveh";
     $scope.message = {
-        author: $scope.nickName,
+        author: "anonimo",
         content: ""
     };
     $scope.messageLog = [];
-
     $scope.sendMessage = function (message) {
-        var splitted = message.content.split(" ");
-        if (splitted[0] == "/nickname") {
-            $scope.nickName = "";
-            for (var i = 0; i < splitted.length; i++) {
-                $scope.nickName += splitted[i + 1];
-            }
+        var matchNick = message.content.match('^\/nick (.*)');
+        var matchServer = message.content.match('^\/server (.*)');
+        if (angular.isDefined(matchNick) && angular.isArray(matchNick) && matchNick.length === 2) {
+            message.author = matchNick[1];
+            $scope.message.content = '';
+        } else if (angular.isDefined(matchServer) && angular.isArray(matchServer) && matchServer.length === 2) {
+            socketService.setServer(matchServer[1]);
             $scope.message.content = "";
         } else {
-            //$scope.messageLog.push(message);
-            socketService.emit('/messages/create', message, $scope.addMessage);
+            socketService.mySocket.emit('/messages/create', message, $scope.addMessage);
             $scope.message.content = "";
         }
     };
-
     $scope.addMessage = function (message) {
         //$scope.messageLog.push(message);
-        $log.debug('se dispara la funcion que recibe como callback, con el mensaje' + message);
-    };
+        $log.debug('se dispara la funcion que recibe como callback, con el mensaje: ' + message.content);
 
-    socketService.forward('newMessage', $scope);
+    };
+    socketService.mySocket.forward('newMessage', $scope);
     $scope.$on('socket:newMessage', function (event, data) {
-        $log.debug('got a message', data.author);
+        $log.debug('got a message from: ', data.author);
+        $log.debug('event: ', event);
         /*if (!data.content) {
-            $log.error('invalid message', 'data', JSON.stringify(data));
-            return;
-        }*/
+         $log.error('invalid message', 'data', JSON.stringify(data));
+         return;
+         }*/
         $scope.$apply(function () {
             $scope.messageLog.push(data);
             //log$scope.messageLog = messageFormatter(new Date(), data.source, data.payload) + $scope.messageLog;
         });
     });
-}]);
+}])
+;
--- app/scripts/services/socketService.js
+++ app/scripts/services/socketService.js
@@ -1,12 +1,17 @@
 'use strict';
 app.factory('socketService', function (socketFactory) {
 
-    var myIoSocket = io.connect('http://192.168.2.109:3000');
+    var myIoSocket = io.connect('http://192.168.31.121:3000');
 
     var mySocket = socketFactory({
         ioSocket: myIoSocket
     });
     mySocket.forward('broadcast');
 
-    return mySocket;
+    return {
+        mySocket : mySocket,
+        setServer : function(direccion){
+            myIoSocket = io.connect(direccion);
+        }
+    };
 });
